127c9ed6f9c3ded98136a7e19fba72ced82af419
yousifonthissite <yousifalnisskibidi@gmail.com>
2025-04-30 10:29:19 +0400

    Updatemain.js

--- config/main.js
+++ config/main.js
@@ -1,7 +1,7 @@
 // This changes the title of your site
 
-var sitename = "native."; // Change this to change the name of your website.
-var subtext = "v1.2"; // set the subtext
+var sitename = "YousifAlnaqbiGames"; // Change this to change the name of your website.
+var subtext = "v1.3"; // set the subtext
 
 // more settings in main.css
 
